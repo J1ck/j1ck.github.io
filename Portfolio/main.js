10ad4f18d00cc9a437fdc6f7d39778e47e159a6b
J1ck <notj1ck@gmail.com>
2024-12-11 17:34:14 +1000

    new portfolio create

---
+++ Portfolio/main.js
@@ -0,0 +1,227 @@
+const IdToHTML = {
+    nsa: `
+    <h1>No-Scope Arcade</h1>
+    <h5>Fast-Paced First Person Shooter</h5>
+
+    <div class="break"></div>
+
+    <h3>Developed and Shipped Key Features:</h3>
+    <ul>
+        <li>Custom Bullet Trails</li>
+        <li>Experience, Levels, and Levelling Rewards</li>
+        <li>Store Revamp with Item Previews, Rotating Daily & Weekly Shops, and Bundles</li>
+        <li>Twitter Code Rewards</li>
+        <li>Moderation System Re-Code using Firebase</li>
+        <li>Nuke Event Revamp</li>
+    </ul>
+
+    <h3>Behind the Scenes:</h3>
+    <ul>
+        <li>Leveraged GitHub and Rojo for seamless version control and integration of code changes across a shared codebase</li>
+        <li>Utilized Kanban workflow via Trello to manage development tasks and milestones</li>
+        <li>Contributed to sprint planning and progress tracking in weekly agile meetings</li>
+        <li>Fixed a multitude of bugs ranging in severity, making sure the experience runs seamlessly and without issue</li>
+        <li>Optimized slow features to accomodate lower-end devices</li> 
+    </ul>
+
+    <div class="break"></div>
+
+    <h3>Game Description</h3>
+
+    <a>No-Scope Arcade is a fast-paced, adrenaline-pumping, movement-based first-person shooter! Run through the battleground arena to squash the competition and come out as the ultimate winner.<br><br>Unlock gun skins, character operators, bullet tracers, kill effects, and melee weapons with the tokens you get rewarded. Evade your enemies with jumping, sliding, and wall-hopping movements to avoid being shot.</a> 
+    <iframe src="https://www.youtube-nocookie.com/embed/PhDJO2WI8bY?si=kRB8y4foTuXUUZb_&amp;controls=0&autoplay=1&mute=1&loop=1&playlist=PhDJO2WI8bY" title="YouTube video player" frameborder="0" allow="autoplay; encrypted-media"></iframe>
+    <div class="square-grid">
+        <img src="https://tr.rbxcdn.com/180DAY-4aa519228ceac6ccaa64ae5effd7b670/768/432/Image/Webp/noFilter">
+        <img src="https://tr.rbxcdn.com/180DAY-a2a938f6af712866e74990498b8ae3d3/768/432/Image/Webp/noFilter">
+        <img src="https://tr.rbxcdn.com/180DAY-87d64201ea75e5599e0cf813596f6adc/768/432/Image/Webp/noFilter">
+        <img src="https://tr.rbxcdn.com/180DAY-df37941192479a74f916609822f307d7/768/432/Image/Webp/noFilter">
+    </div>
+    `,
+    be: `
+    <h1>Blood Engine</h1>
+    <h5>Realistic Sandbox Shooter</h5>
+
+    <div class="break"></div>
+
+    <h3>Developed Key Features:</h3>
+    <ul>
+        <li>Loadout System Rework and Saving</li>
+        <li>Completely New Sandbox Map with New Interactive Machines</li>
+        <li>Picking Up and Throwing Ragdolls & Objects</li>
+        <li>Follow the Developers Rewards</li>
+        <li>Action Keybind Hints Rework</li>
+    </ul>
+
+    <h3>Behind the Scenes:</h3>
+    <ul>
+        <li>Fixed <i>a lot</i> of bugs, making the experience a lot more stable</li>
+        <li>Slightly improved performance by refactoring some inefficient code and removing some performance-heavy visuals</li>
+    </ul>
+
+    <div class="break"></div>
+
+    <h3>Game Description</h3>
+
+    <a>Known for having some of the most advanced blood/gore and ragdoll physics on Roblox, take a dive into the immersive experience of Blood Engine and battle others in multiplayer, or execute testing subjects in sandbox mode in the most brutal ways possible.</a>
+    <div class="square-grid">
+        <img src="https://tr.rbxcdn.com/180DAY-876a9bd4513623e15e5e0e84e9134417/768/432/Image/Webp/noFilter">
+    </div>
+    `,
+    p1v1: `
+    <h1>Pistol 1v1</h1>
+    <h5>Competitive 1v1 First Person Shooter</h5>
+
+    <div class="break"></div>
+
+    <h3>Behind the Scenes:</h3>
+    <ul>
+        <li>Fixed a multitude of bugs, making the experience more stable for everyone</li>
+        <li>Optimized the experience to lessen the network load to shorten load times</li>
+    </ul>
+
+    <div class="break"></div>
+
+    <h3>Game Description</h3>
+
+    <a>Pistol 1v1 is a ranked competitive 1-versus-1 game centered around pistol-based weapons. Your goal is simple: win games, rank up, unlock skins, and repeat. Rack up your rank and become the king of pistols!</a>
+    <div class="square-grid">
+        <img src="https://tr.rbxcdn.com/180DAY-92c80dd44e2fbf6da3a8792451b3af81/768/432/Image/Webp/noFilter">
+    </div>
+    `,
+    vesteria: `
+    <h1>Vesteria</h1>
+    <h5>Open-World MMO-RPG</h5>
+
+    <div class="break"></div>
+
+    <h3>Behind the Scenes:</h3>
+    <ul>
+        <li>Swiftly adapted to an extensive and intricate codebase, built and maintained by dozens of programmers</li>
+        <li>Made use of proprietary tools to design, develop, and implement new features and content</li>
+    </ul>
+
+    <div class="break"></div>
+
+    <h3>Game Description</h3>
+
+    <a>Vesteria is an open-world MMORPG exclusively on Roblox. Triumph over Bosses, pillage Dungeons, complete Quests, and find your place in the world of Vesteria.</a>
+    <div class="square-grid">
+        <img src="https://tr.rbxcdn.com/180DAY-faba828a788ffa70b4b8d528ad3fe1fa/768/432/Image/Webp/noFilter">
+        <img src="https://tr.rbxcdn.com/180DAY-1bf2b624e478c95314ed37f1eec34cc9/768/432/Image/Webp/noFilter">
+        <img src="https://tr.rbxcdn.com/180DAY-a7fb80dc5498c1f3a8ec5ab178320c24/768/432/Image/Webp/noFilter">
+        <img src="https://tr.rbxcdn.com/180DAY-b130d52f34c6c5a181b92b984126d5d5/768/432/Image/Webp/noFilter">
+        <img src="https://tr.rbxcdn.com/180DAY-010809fb12d78f6c8c95c75623ca33f4/768/432/Image/Webp/noFilter">
+        <img src="https://tr.rbxcdn.com/180DAY-6905d9efc9c97dc528f452b20b25264f/768/432/Image/Webp/noFilter">
+    </div>
+    `,
+    bd: `
+    <h1>Better Dance?</h1>
+    <h5>Round-Based Pick-a-Side Spinoff</h5>
+
+    <div class="break"></div>
+
+    <h3>Developed Key Features:</h3>
+    <ul>
+        <li>Entire Codebase Designed and Developed from Scratch</li>
+        <li>Leaderboards Displaying Kills, Wins, and Cash</li>
+        <li>Gacha System for Weapon Cosmetics</li>
+        <li>Potion System to Buff a User for a Limited Time</li>
+        <li>Round System to Place Players on a Team and Determine the Winners</li>
+        <li>AFK Zone to Idly Give Players Cash</li>
+        <li>Entire User Interfaced Designed, Developed, and Implemented from Scratch</li>
+    </ul>
+
+    <h3>Behind the Scenes:</h3>
+    <ul>
+        <li>Worked with testers to find and fix intricate bugs efficiently</li>
+        <li>Reworked the game theme to Fortnite emotes instead of Roblox emotes</li>
+        <li>Used personal proprietary tools to develop the game quickly and ensure stability</li>
+        <li>Made use of MemoryStoreService to prompt players to teleport to a larger server when one was available</li>
+    </ul>
+
+    <div class="break"></div>
+
+    <h3>Game Description</h3>
+
+    <a>Choose between YOUR favorite dances and fight over your taste on the dance floor. Choose your side: 1 of 2 Dances. Knock out other players and defend your dance to the death!</a>
+    <div class="square-grid">
+        <img src="https://tr.rbxcdn.com/180DAY-0bc151e340f6920c44613501f5c078ba/768/432/Image/Webp/noFilter">
+    </div>
+    `,
+    dm: `
+    <h1>Disaster Masters Alpha</h1>
+    <h5>Round-Based Arcade Minigame</h5>
+
+    <div class="break"></div>
+
+    <h3>Developed Key Features:</h3>
+    <ul>
+        <li>anti-cheat</li>
+        <li>disaster system</li>
+        <li>greedy meshed voxel destruction</li>
+        <li></li>
+    </ul>
+
+    <h3>Behind the Scenes:</h3>
+    <ul>
+        
+    </ul>
+
+    <div class="break"></div>
+
+    <h3>Game Description</h3>
+
+    <a>Be the first to get the disaster totem to become the disaster master and wreak havoc on all of the survivors, or else perish! Scramble around and avoid the insane disasters the disaster master has to throw your way. Fling yourself across the map to dodge destruction, or if you’re the master, destroy everything!</a>
+    
+    <iframe src="https://www.youtube-nocookie.com/embed/ktffMYj1Glk?si=Kvm5IsFCxnIjw27j&amp;controls=0&autoplay=1&mute=1&loop=1&playlist=ktffMYj1Glk" title="YouTube video player" frameborder="0" allow="autoplay; encrypted-media"></iframe>
+    <div class="square-grid">
+        <img src="https://tr.rbxcdn.com/180DAY-0dcf396a5963feb93a845a14cd1e57cc/768/432/Image/Webp/noFilter">
+    </div>
+    ` // 
+};
+
+const IdToGameLink = {
+    bd: "18986150790",
+    vesteria: "2376885433",
+    nsa: "6407649031",
+    be: "13488637865",
+    p1v1: "6722284015",
+    dm: "14963990817"
+};
+
+const Description = document.querySelector("#description");
+
+function OpenDescription(Id) {
+    document.querySelector("#description-temp")?.remove();
+
+    // document.querySelector("#main")?.removeAttribute("style");
+
+    document.querySelector("#game-link").setAttribute("href", `https://roblox.com/games/${IdToGameLink[Id]}`);
+
+    Description.removeAttribute("hidden");
+    Description.setAttribute("style", `
+        width: 70ch;
+    `);
+
+    const Body = document.createElement("div");
+    Body.setAttribute("class", "inner-body");
+    Body.setAttribute("id", "description-temp");
+    Body.innerHTML = IdToHTML[Id].trim();
+
+    Description.appendChild(Body);
+}
+
+function CloseDescription() {
+    Description.setAttribute("hidden", true);
+    Description.setAttribute("style", `
+        width: 0ch;
+    `);
+
+    document.querySelector("#description-temp")?.remove();
+
+    // document.querySelector("#main")?.setAttribute("style", "margin-right: auto;");
+}
+
+window.onload = () => {
+    CloseDescription();
+};
